95c74be926e4989738de1ebe59880872a1643e86
kneher99 <kneher@gmail.com>
2014-12-31 14:45:16 -0500

    First Commit

---
+++ AcceptingInput/AcceptingInput/AcceptingInput.py
@@ -0,0 +1,19 @@
+#name = input("What is your name?")
+
+#print (name + " is learning Python.")
+
+country = input("What country do you live in?")
+country = country.upper()
+print("You live in " + country)
+print(country.find('S'))
+#message = 'Hello World'
+#print(message.lower())
+#print(message.upper())
+#print(message.swapcase())
+
+#num1 = 3
+#num2 = 4
+
+#num3 = num1 + num2
+
+#print(num3)
---
+++ Module2DisplayingText/Module2DisplayingText/Module2DisplayingText.py
@@ -0,0 +1,5 @@
+print('The capybara is the world\'s largest rodent.')
+print("It likes to live in groups")
+
+print('here is a double quote"' + " here is a single quote'")
+print("or you can just do this \" does that work")
